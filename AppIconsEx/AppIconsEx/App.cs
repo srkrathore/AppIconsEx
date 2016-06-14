9d5a844912ad2c3b20a84328bf405d54eab6d6b0
Rathore <Rathore@DESKTOP-QTI5N0N>
2016-06-14 10:26:43 +0530

    Add project files.

---
+++ AppIconsEx/AppIconsEx/App.cs
@@ -0,0 +1,41 @@
+﻿
+using Xamarin.Forms;
+
+namespace AppIconsEx
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms! \n Application Icon Setting Example Project"
+                        }
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
